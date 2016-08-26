813ed982ab204d6b314ed614819f393d0f27ec0a
David Guan <davidguandev@gmail.com>
2016-08-26 14:48:10 +0800

    Clean code.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,17 @@
 import React from 'react'
-import Form from 'react-jsonschema-form';
+import Form from 'react-jsonschema-form'
 
+import { defaultMarkdownText, defaultSchemaText } from '../constants/defaultText'
 import 'normalize.css/normalize.css'
 import 'styles/main.scss'
 
-const ERROR_BORDER = '1px solid red'
-const defaultMarkdown =
-`---
-user: echo
-title: Echo Hu
-position: Operations Assistant
-featured: true
----`
-
-const defaultSchema =
-`[{
-  "name": "ref", "target": "user", "type": "string"
-},{
-  "name": "name", "target": "title", "type": "string"
-}, {
-  "name": "position", "target": "position", "type": "string"
-}, {
-  "name": "featured", "target": "featured", "type": "boolean"
-}]`
 
 class AppComponent extends React.Component {
   constructor() {
     super()
     this.state = {
       schemaCanBeParsed: true,
-      markdownText: defaultMarkdown,
+      markdownText: defaultMarkdownText,
       formSchema: { type: 'object', properties: {} },
       previousMarkdownState: '',
       resultMarkdown: ''
@@ -50,6 +32,7 @@ class AppComponent extends React.Component {
         for (let i = 0; i < obj.length; i++) {
           if(obj[i].type !== 'boolean' && obj[i].type !== 'string') {
             schemaCanBeParsed = false
+            break
           }
         }
       }
@@ -120,19 +103,24 @@ class AppComponent extends React.Component {
   }
 
   render() {
-    const { formSchema, markdownText, resultMarkdown, schemaCanBeParsed } = this.state
+    const {
+      formSchema,
+      markdownText,
+      resultMarkdown,
+      schemaCanBeParsed
+    } = this.state
 
     return (
       <div>
         <h3>Schema</h3>
         <textarea
           cols='50'
-          defaultValue={defaultSchema}
+          defaultValue={defaultSchemaText}
           name='textarea'
           onChange={() => this.checkSchema()}
           ref='schemaInput'
           rows='10'
-          style={{ border: schemaCanBeParsed ? '' : ERROR_BORDER }}
+          className={ schemaCanBeParsed ? '' : 'error' }
         />
         <h3>Original Markdown</h3>
         <textarea
@@ -144,11 +132,18 @@ class AppComponent extends React.Component {
           onChange={() => this.updateMarkdown()}
         />
         <h3>Edit From</h3>
-        <Form schema={formSchema}
+        <Form
           onSubmit={res => this.updateResult(res.formData)}
+          schema={formSchema}
         />
         <h3>Result</h3>
-        <textarea name='textarea' rows='10' cols='50' ref='res' value={resultMarkdown} />
+        <textarea
+          cols='50'
+          name='textarea'
+          rows='10'
+          ref='res'
+          value={resultMarkdown}
+        />
       </div>
     )
   }
