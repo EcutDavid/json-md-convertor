bce342994d3c8309e845f7c1a8f4cf3221e55882
David Guan <davidguandev@gmail.com>
2016-08-26 11:27:04 +0800

    Add placeholders.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import 'normalize.css/normalize.css'
 
-import 'styles/app.scss'
+import 'normalize.css/normalize.css'
+import 'styles/main.scss'
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <button className="button">Hello human</button>
+      <div>
+        <h3>Schema</h3>
+        <textarea name="textarea" rows="10" cols="50" defaultValue='21312' />
+        <h3>Original Markdown</h3>
+        <textarea name="textarea" rows="10" cols="50" />
+        <h3>Edit From</h3>
+        <h3>Result</h3>
+        <textarea name="textarea" rows="10" cols="50" />
       </div>
     )
   }
